5dc1106f670a363914600bdc2a94eebd39e462d4
Philip <mrphilipowade@gmail.com>
2024-09-06 22:02:04 +0300

    2 commit

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -1,19 +1,7 @@
 'use client'
 // import node module libraries
 import { Fragment } from "react";
-import Link from 'next/link';
 import { Container, Col, Row } from 'react-bootstrap';
-
-// import widget/custom components
-import { StatRightTopIcon } from "widgets";
-
-// import sub components
-import { ActiveProjects, Teams,
-    TasksPerformance
-} from "sub-components";
-
-// import required data files
-import ProjectsStatsData from "data/dashboard/ProjectsStatsData";
 import PieChart from "./charts/PieChart";
 import CandlestickChart from "./charts/CandlestickChart";
 import BarChart from "./charts/BarChart";
